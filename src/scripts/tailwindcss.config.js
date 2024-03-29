5e752b18af0ea012ba711474b820792a58c91ddb
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2024-03-29 10:49:39 -0300

    finalizado

--- src/scripts/tailwindcss.config.js
+++ src/scripts/tailwindcss.config.js
@@ -1,9 +1,13 @@
 tailwind.config = {
     theme: {
+      fontFamily: {
+          'Inter': ['inter', 'sans-serif']
+      },
       extend: {
         colors: {
           cor0: '#303030',
-          cor1: '#F9F9F9'
+          cor1: '#F9F9F9',
+          cor2: '#C07212'
         }
       }
     }
